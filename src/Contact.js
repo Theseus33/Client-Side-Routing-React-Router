8cf4f1bfdea5cdce68d91bd9bbafe8522bfa4c27
Rana Bhattacharyya <ranab33@gmail.com>
2019-10-24 18:34:24 -0400

    added adition pages

---
+++ src/Contact.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+    render() {
+        return (
+            <div className='Contact'>
+                <h1>Contact!!!</h1>
+                <p>Olease dont contact us ever.</p>
+            </div>
+        );
+    }
+}
+
+export default Contact;
---
+++ src/Dog.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Dog extends Component {
+    render() {
+        return (
+            <div className='Dog'>
+                <h1>Dog!!!</h1>
+                <img src='https://i.imgur.com/nnhQbpY.jpg' />
+            </div>
+        );
+    }
+}
+
+export default Dog;
