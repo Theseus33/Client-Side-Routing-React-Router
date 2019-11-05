a2fffaa4ee319013e584f8877f1f95d386708e10
Rana Bhattacharyya <ranab33@gmail.com>
2019-11-04 21:16:54 -0500

    still fake routing with href

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import About from './About';
 import Dog from "./Dog";
 import Contact from "./Contact";
 import "./App.css"
 
+const Hater = () => <h1>I ABSOLUTELY HATE DOGS!</h1>
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <Route path="/dog" component={Dog} />
+        <nav className='App-nav'>
+          <a href="/dog">Dog</a>
+          <a href="/contact">Contact</a>
+          <a href="/">Aout</a>
+        </nav>
+        <Switch>
+          <Route exact path="/" component={About} />
+          <Route path="/dog" component={Dog} />
+          <Route path="/dog/hater" component={Hater} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
       </div>
     )
   }
